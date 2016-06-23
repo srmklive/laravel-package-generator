22fca7360fb7af3e9fbe682ff5782ed76c4443e6
Raza Mehdi <srmk@outlook.com>
2016-06-23 15:44:57 +0500

    Add service provider

---
+++ src/Commands/PackageGeneratorCommand.php
@@ -0,0 +1,28 @@
+<?php namespace Srmklive\PackageGenerator\Commands;
+
+use Illuminate\Console\Command as ConsoleCommand;
+
+class PackageGeneratorCommand extends ConsoleCommand
+{
+    /**
+     * The console command name.
+     * @var string
+     */
+    protected $name = 'make:package';
+
+    /**
+     * The console command description.
+     * @var string
+     */
+    protected $description = 'Generate your own custom packages for use in Laravel applications.';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+
+    }
+}
---
+++ src/Providers/PackageGeneratorServiceProvider.php
@@ -0,0 +1,35 @@
+<?php namespace Srmklive\PackageGenerator;
+
+use Illuminate\Support\ServiceProvider;
+use Srmklive\PackageGenerator\Commands\PackageGeneratorCommand;
+
+class PackageGeneratorServiceProvider extends ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->commands([
+            PackageGeneratorCommand::class
+        ]);
+    }
+}
