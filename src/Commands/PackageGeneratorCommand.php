75a87acb289e46eb40a858ded2b92e5d34c0a15c
Raza Mehdi <srmk@outlook.com>
2016-06-23 19:53:12 +0500

    Added stubs

--- src/Commands/PackageGeneratorCommand.php
+++ src/Commands/PackageGeneratorCommand.php
@@ -15,7 +15,7 @@ class PackageGeneratorCommand extends ConsoleCommand
      * The console command description.
      * @var string
      */
-    protected $description = 'Generate your own custom packages for use in Laravel applications.';
+    protected $description = 'Create your own custom packages.';
 
     /**
      * Execute the console command.
@@ -24,13 +24,45 @@ class PackageGeneratorCommand extends ConsoleCommand
      */
     public function handle()
     {
-        $author = $this->input->getOption('author');
+        $author = $this->option('author');
+        $name = $this->option('name');
+        $email = $this->option('email');
+        $package = $this->option('package');
 
-        $name = $this->input->getOption('name');
+        // Make sure that package name must consist only underscore & dash for symbols
+        if (! preg_match('/^[A-Za-z\\-\\_]{1,}$/i', $package)) {
+            throw new \Exception('Name must contain only alphabets, underscore & dash!');
+        }
 
-        $namespace = $this->input->getOption('namespace');
+        $namespace = $this->option('namespace');
+        if (empty($namespace)) {
+            $namespace = $this->generateNamespace($author, $package);
+        }
+        
+        
+    }
 
+    /**
+     * Generate namespace for package.
+     *
+     * @param $author
+     * @param $name
+     * @return string
+     */
+    protected function generateNamespace($author, $name)
+    {
+        $namespace = ucfirst($author) . '\\';
 
+        $items = explode('-', preg_replace('/([-_])/', '-', $name));
+        if (!empty($items)) {
+            foreach ($items as $key => $item) {
+                $items[$key] = ucfirst($item);
+            }
+        }
+
+        $name = str_replace('-', '', implode('-', $items));
+
+        return $namespace . $name;
     }
 
     /**
@@ -42,8 +74,12 @@ class PackageGeneratorCommand extends ConsoleCommand
     {
         return [
             ['author', null, InputOption::VALUE_REQUIRED, 'Package author id (e.g. Acme).'],
-            ['name', null, InputOption::VALUE_REQUIRED, 'Package name (e.g. my-package).'],
-            ['namespace', null, InputOption::VALUE_OPTIONAL, 'Namespace to be used in Package code.']
+            ['name', null, InputOption::VALUE_REQUIRED, 'Package author name. (e.g. John Doe)'],
+            ['email', null, InputOption::VALUE_REQUIRED, 'Package author email. (e.g. johndoe@example.com)'],
+            ['package', null, InputOption::VALUE_REQUIRED, 'Package name (e.g. my-package).'],
+            ['namespace', null, InputOption::VALUE_OPTIONAL, 'Namespace to be used in Package code.'],
+            ['description', null, InputOption::VALUE_OPTIONAL, 'Package description.'],
+            ['keywords', null, InputOption::VALUE_OPTIONAL, 'Package keywords.'],
         ];
     }
 }
---
+++ stubs/README.md
@@ -0,0 +1,2 @@
+# DummyPackageName
+DummyPackageDescription
