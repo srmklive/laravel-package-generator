b7a45852d82f1558d05a56d4b6a2fb218051938b
Raza Mehdi <srmk@outlook.com>
2016-07-04 16:23:36 +0500

    Create custom package through console command

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# laravel-packages-generator
-This package can be used create custom packages for use in Laravel applications.
+# Laravel Package Generator
+
+- [Introduction](#introduction)
+- [Installation](#installation)
+- [Usage](#usage)
+
+<a name="introduction"></a>
+## Introduction
+
+By using this package, you can create custom packages for your laravel applications. 
+
+**Only Laravel 5.1 or greater is supported in this package.**
+
+
+<a name="installation"></a>
+## Installation
+
+* Use following command to install:
+
+```
+composer require srmklive/laravel-package-generator
+```
+
+* Add the service provider to your $providers array in config/app.php file like: 
+
+```
+Srmklive\PackageGenerator\Providers\PackageGeneratorServiceProvider::class
+```
+
+<a name="usage"></a>
+## Usage
+
+```
+php artisan make:package --author=author --name="Test Author" --email=test@example.com --package=mypackage
+```
--- src/Commands/PackageGeneratorCommand.php
+++ src/Commands/PackageGeneratorCommand.php
@@ -15,12 +15,12 @@ class PackageGeneratorCommand extends ConsoleCommand
      * The console command description.
      * @var string
      */
-    protected $description = 'Create your own custom packages.';
+    protected $description = 'Create new custom package.';
 
     /**
      * Execute the console command.
      *
-     * @return mixed
+     * @throws \Exception
      */
     public function handle()
     {
@@ -28,6 +28,8 @@ class PackageGeneratorCommand extends ConsoleCommand
         $name = $this->option('name');
         $email = $this->option('email');
         $package = $this->option('package');
+        $description = $this->option('description');
+        $keywords = $this->option('keywords');
 
         // Make sure that package name must consist only underscore & dash for symbols
         if (! preg_match('/^[A-Za-z\\-\\_]{1,}$/i', $package)) {
@@ -38,8 +40,30 @@ class PackageGeneratorCommand extends ConsoleCommand
         if (empty($namespace)) {
             $namespace = $this->generateNamespace($author, $package);
         }
-        
-        
+
+        $vendor_dir = base_path('vendor');
+        $package_dir = $vendor_dir . "/$author/$package";
+        $stubs_dir = realpath(dirname(__FILE__) . '/../../stubs');
+
+        // Create package directory
+        if (! is_dir($package_dir)) {
+            @mkdir($package_dir, 0777, true);
+            @chmod($package_dir, 0777);
+        }
+
+        $content = [
+            'author' => $author,
+            'package' => $package,
+            'name' => $name,
+            'email' => $email,
+            'description' => $description,
+            'class' => ucfirst($package),
+            'namespace' => $namespace,
+            'packagenamespace' => addslashes($namespace)
+        ];
+
+        // Copy source code files
+        $this->createPackageSource($stubs_dir, $package_dir, $content);
     }
 
     /**
@@ -63,6 +87,87 @@ class PackageGeneratorCommand extends ConsoleCommand
         $name = str_replace('-', '', implode('-', $items));
 
         return $namespace . $name;
+    }
+
+    /**
+     * Copy package source files.
+     *
+     * @param $source
+     * @param $destination
+     * @param $content
+     * @param string $extension
+     */
+    private function createPackageSource($source, $destination, $content, $extension = '')
+    {
+        $source_dir = new \DirectoryIterator($source);
+
+        if (! is_dir($destination))
+            @mkdir($destination, 0777, true);
+
+        $replace = [
+            'DummyAuthorID' => 'author',
+            'DummyPackageName' => 'package',
+            'DummyAuthorName' => 'name',
+            'DummyAuthorEmail' => 'email',
+            'DummyPackageDescription' => 'description',
+            'DummyClassName' => 'class',
+            'DummyNameSpace' => 'namespace',
+        ];
+
+        foreach ($source_dir as $item) {
+            // Skip dot items
+            if ($item->isDot())
+                continue;
+
+            $tmp_destination = str_replace($source, $destination, $item->getPathname());
+
+            if ($item->isFile()) {
+                if (!empty($extension))
+                    $tmp_destination = str_replace('.stub', $extension, $tmp_destination);
+
+                $tmp_file = basename($tmp_destination);
+
+                if (in_array($tmp_file, ['ServiceProvider.php','Class.php','FacadeAccessor.php'])) {
+                    if (in_array($tmp_file, ['ServiceProvider.php','FacadeAccessor.php']))
+                        $tmp_file = "$content[class]" . $tmp_file;
+                    else
+                        $tmp_file = "$content[class].php";
+
+                    $tmp_destination = str_replace(
+                        basename($tmp_destination),
+                        $tmp_file,
+                        $tmp_destination
+                    );
+                }
+
+                copy($item->getPathname(), $tmp_destination);
+                @chmod($tmp_destination, 0777);
+
+                $file_content = file_get_contents($tmp_destination);
+                foreach ($replace as $key => $value) {
+                    if (!empty($content[$value]))
+                        $file_content = str_replace($key, $content[$value], $file_content);
+                }
+
+                file_put_contents($tmp_destination, $file_content);
+            }
+
+            if ($item->isDir()) {
+
+                $item_dir = str_replace(
+                    $source,
+                    $destination,
+                    $item->getPathname()
+                );
+
+                $this->createPackageSource(
+                    $item->getPathname(),
+                    $item_dir,
+                    $content,
+                    '.php'
+                );
+            }
+        }
     }
 
     /**
