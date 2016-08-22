a21015711612297ed9f5ae8c0c7f6e4c430a5477
Raza Mehdi <srmk@outlook.com>
2016-08-22 16:20:13 +0500

    Created installer

---
+++ src/Commands/NewCommand.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Srmklive\PackageGenerator\Commands;
+
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Command\Command as SymfonyCommand;
+
+class NewCommand extends SymfonyCommand
+{
+    /**
+     * The input interface.
+     *
+     * @var InputInterface
+     */
+    public $input;
+
+    /**
+     * The output interface.
+     *
+     * @var OutputInterface
+     */
+    public $output;
+
+    /**
+     * Configure the command options.
+     *
+     * @return void
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('new')
+            ->setDescription('Create a new composer package')
+            ->addArgument('name', InputArgument::REQUIRED, 'The name of the package')
+            ->addOption('laravel', null, InputOption::VALUE_NONE, 'Create Laravel specific package');
+    }
+
+    /**
+     * Execute the command.
+     *
+     * @param  InputInterface  $input
+     * @param  OutputInterface  $output
+     * @return void
+     */
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->input = $input;
+        $this->output = new SymfonyStyle($input, $output);
+    }
+}
